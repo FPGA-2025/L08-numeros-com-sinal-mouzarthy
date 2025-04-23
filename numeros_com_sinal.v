6e86f29c66e4cdd30bf5b43d8c2655b419d6c102
Mouzarthy <mouzarthy@gmail.com>
2025-04-23 00:10:28 -0300

    tarefa numeros com sinal

--- numeros_com_sinal.v
+++ numeros_com_sinal.v
@@ -8,5 +8,35 @@ module numeros_com_sinal(
 );
 
 //insira seu codigo aqui
+always @(*) begin
+        // case(codigo)
+        // 'b00: saida = entrada_signed_1 + entrada_signed_2;
+        // 'b01: saida = entrada_unsigned_1 + entrada_unsigned_2;
+        // 'b10: saida = entrada_unsigned_1 + entrada_signed_1;
+        // 'b11: saida = entrada_unsigned_1 + entrada_signed_2;
+        // default: saida = 8'b0;
+    // endcase
+
+    if(codigo == 2'b00)
+    begin
+         saida = entrada_signed_1 + entrada_signed_2;
+    end
+    else if(codigo == 2'b01)
+    begin
+        saida = entrada_unsigned_1 + entrada_unsigned_2;
+    end
+    else if(codigo == 2'b10)
+    begin
+        saida = entrada_unsigned_1 + entrada_signed_1;
+    end
+    else if(codigo == 2'b11)
+    begin
+        saida = entrada_unsigned_1 + entrada_signed_2;
+    end
+    else
+    begin
+         saida = 0;
+    end
+end
 
 endmodule
